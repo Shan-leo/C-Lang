ca62fff07d55bf5670d11e510e9107247c6e4b00
Shan-leo <bizwithshan@gmail.com>
2023-02-04 22:07:54 +0530

    end

--- memoryAddresses.c
+++ memoryAddresses.c
@@ -4,10 +4,13 @@
 int main(){
 
     int age = 30;
+    int * pAge = &age; //This is a pointer variable
     double gpa=3.2;
+    double * pGpa = &gpa;
     char grade = 'A';
+    char * pgrade = &grade;
 
-    
+    printf("%d\n", *pAge); //  this is dereferrencing the pointer variable
 
     return 0;
 }
